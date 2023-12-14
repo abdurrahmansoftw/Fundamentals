a1b74cf07c7f446300426cffb8a0882a00da39c6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-14 17:04:36 +0600

    Updating Array of Objects

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,36 @@ function App() {
   //   setGame({ ...game, player: { ...game, player: { ...game.player, name: 'akaid' } } });
   // };
 
-  const [pizza, setPizza] = useState({
-    name: 'Pepperoni',
-    toppings: ['Pepperoni'],
-  });
+  // const [pizza, setPizza] = useState({
+  //   name: 'Pepperoni',
+  //   toppings: ['Pepperoni'],
+  // });
+
+  // const handleClick = () => {
+  //   setPizza({ ...pizza, toppings: [...pizza.toppings, 'Bacon'] });
+  // };
+
+  // const [cart, setCart] = useState({
+  //   discount: 0.2,
+  //   items: [
+  //     { id: 1, name: 'Pizza', price: 10, quantity: 1 },
+  //     { id: 2, name: 'Burger', price: 5, quantity: 2 },
+  //   ],
+  // });
+
+  // const handleCart = () => {
+  //   setCart({ ...cart, items: cart.items.map((item) => ({ ...item, price: item.price * (1 - cart.discount) })) });
+  // };
+
+  const [bugs, setBugs] = useState([
+    { id: 1, name: 'Ant', fixed: true },
+    { id: 2, name: 'Grasshopper', fixed: false },
+    { id: 3, name: 'Ladybug', fixed: false },
+  ]);
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, 'Bacon'] });
+    // set bugs arnary  operator to check if id = 2 then fixed = true else fixed = false and return bugs array with updated values using map
+    setBugs(bugs.map((bug) => (bug.id === 2 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
