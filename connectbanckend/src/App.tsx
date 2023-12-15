8026ed3c6fd28cb5d0b24c3d47b9105137f50dda
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 18:04:39 +0600

    Promise to fetch data

--- connectbanckend/src/App.tsx
+++ connectbanckend/src/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react';
 import ProductList from './components/ProductList';
 
 const App = () => {
-  const [category, setCategory] = useState('');
-
   return (
-    <div className='container p-3'>
-      <select className='form-select' onChange={(e) => setCategory(e.target.value)}>
-        <option value=''></option>
-        <option value='Clothing'>Clothing</option>
-        <option value='Household'>Household</option>
-      </select>
-      <ProductList category={category} />
+    <div>
+      <ProductList />
     </div>
   );
 };
