f85ced0e767dd78a3fe6d84ec8a586d68b3e677e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 23:59:57 +0600

    update

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,19 +1,5 @@
-import { Button, Stack } from '@chakra-ui/react';
-import { MdBuild , MdCall } from "react-icons/md"
-
 function App() {
-  return (
-    <div>
-      <Stack direction='row' spacing={4}>
-        <Button leftIcon={<MdBuild />} colorScheme='pink' variant='solid'>
-          Settings
-        </Button>
-        <Button rightIcon={<MdCall />} colorScheme='blue' variant='outline'>
-          Call us
-        </Button>
-      </Stack>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
