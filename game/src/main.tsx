fe74d6206c74b457d173b8f7c5db998b189f7fd3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 23:55:11 +0600

    add theme

--- game/src/main.tsx
+++ game/src/main.tsx
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
+  brand: { 900: '#1a365d', 800: '#153e75', 700: '#2a69ac' },
 };
 
 const theme = extendTheme({ colors });
