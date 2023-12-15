4e605333b7f976cade389edcf84be3ff09a472ba
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 21:27:34 +0600

    Error handle

--- connectbanckend/src/App.tsx
+++ connectbanckend/src/App.tsx
@@ -8,17 +8,19 @@ interface User {
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [errer, setError] = useState('');
 
   useEffect(() => {
     axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .get<User[]>('https://jsonplaceholder.typicode.com/user')
       .then((res) => setUsers(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <div>
       <h1>UserList</h1>
+      {errer && <p className='text-danger'>{errer}</p>}
       <ol>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
