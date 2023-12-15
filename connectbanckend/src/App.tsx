de6f00fb612c409c2468ca9ad58ac1a8da4224a3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 21:22:14 +0600

    data fetching axios

--- connectbanckend/src/App.tsx
+++ connectbanckend/src/App.tsx
@@ -1,9 +1,29 @@
-import ProductList from './components/ProductList';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      <ProductList />
+      <h1>UserList</h1>
+      <ol>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>
     </div>
   );
 };
