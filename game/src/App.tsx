158f17350155be011164a3678f86f5b71db391f7
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-16 00:08:09 +0600

    Build a responsive layout

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,9 +1,23 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Grid>
-      <GridItem>hello</GridItem>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "main aside"`,
+      }}>
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='blue'>
+        Main
+      </GridItem>
     </Grid>
   );
 }
