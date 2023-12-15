d8c0a4473fb34e6b14c76726767f499ced7a4a91
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 23:54:45 +0600

    Installing Chakra UI

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from '@chakra-ui/react';
 function App() {
   return (
     <div>
-      {/* <Button colorScheme='blue'>Button</Button> */}
+      <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
