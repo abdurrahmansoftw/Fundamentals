ecd77bac659f93d96a079efb76ef75269eef52d9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-15 21:44:55 +0600

    deleteUser

--- connectbanckend/src/App.tsx
+++ connectbanckend/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
 
 interface User {
@@ -8,22 +8,52 @@ interface User {
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [errer, setError] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
     axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/user')
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios.delete('https://jsonplaceholder.typicode.com/users/' + user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
   return (
-    <div>
+    <div className='container'>
       <h1>UserList</h1>
       {errer && <p className='text-danger'>{errer}</p>}
-      <ol>
+      {isLoading && <p className='text-primary'>Loading...</p>}
+      <ol className='list-group'>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className='list-group-item d-flex justify-content-between'>
+            {user.name}
+
+            <button className='btn btn-sm btn-outline-danger' onClick={() => deleteUser(user)}>
+              Delete{' '}
+            </button>
+          </li>
         ))}
       </ol>
     </div>
