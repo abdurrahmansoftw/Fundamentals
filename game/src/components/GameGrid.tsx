eb98567dc379e7bd9d21e4aafb3e7bf41f84c4a3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-16 14:04:30 +0600

    SimpleGrid

--- game/src/components/GameGrid.tsx
+++ game/src/components/GameGrid.tsx
@@ -1,16 +1,16 @@
+import { SimpleGrid } from '@chakra-ui/react';
 import useGames from '../hooks/userGames';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
-    <div>
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} padding={5} spacing={10}>
       {error && <p>{error}</p>}
-      <ul>
-        {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
-        ))}
-      </ul>
-    </div>
+      {games.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </SimpleGrid>
   );
 };
 
