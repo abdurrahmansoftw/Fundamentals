7d64ad68f8a5a3c025df9d86a9acf477da27be3f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-16 11:21:48 +0600

    Theme Mood

--- game/src/main.tsx
+++ game/src/main.tsx
@@ -1,17 +1,13 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-
-const colors = {
-  brand: { 900: '#1a365d', 800: '#153e75', 700: '#2a69ac' },
-};
-
-const theme = extendTheme({ colors });
+import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode.dark} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
