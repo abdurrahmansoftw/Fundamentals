669f91975570157ca2e429fd055727f0296d5871
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-16 11:28:05 +0600

    Build ColorMood Switch

--- game/src/components/Navbar.tsx
+++ game/src/components/Navbar.tsx
@@ -1,13 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorMoodSwitch from './ColorMoodSwitch';
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Image src={logo} boxSize='60px' />
-      <Text fontSize='2xl' fontWeight='bold'>
-        Navbar
-      </Text>
+      <ColorMoodSwitch />
     </HStack>
   );
 };
