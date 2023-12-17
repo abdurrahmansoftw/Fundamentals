e307ed524108ae1f248bc6ac75640f212658dd6f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-17 15:09:52 +0600

    Customizing the Chakra Theme

--- game/src/theme.ts
+++ game/src/theme.ts
@@ -10,6 +10,22 @@ const config = {
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#ebf4fa',
+      100: '#d1dae1',
+      200: '#b4c1cb',
+      300: '#97a7b5',
+      400: '#798ea0',
+      500: '#5f7486',
+      600: '#4a5b69',
+      700: '#34414c',
+      800: '#1e272f',
+      900: '#050e15',
+    },
+  },
+});
 
 export default theme;
