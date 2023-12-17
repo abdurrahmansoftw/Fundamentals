daff1c065a3b8ffd388ed571e20a999da74e8717
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-17 23:02:13 +0600

    Install and Import the Devtools

--- React Quary/src/main.tsx
+++ React Quary/src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
