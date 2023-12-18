d4bd989a6a1aa05acc834da95256d6ac409a5aaa
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-18 17:18:58 +0600

    optimistic update

--- React Quary/src/react-query/TodoForm.tsx
+++ React Quary/src/react-query/TodoForm.tsx
@@ -3,21 +3,35 @@ import axios from 'axios';
 import { useRef } from 'react';
 import { Todo } from '../hooks/useTodos';
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo).then((response) => response.data),
-    onSuccess: (savedTodo, newTodo) => {
-      // console.log('savedTodo', savedTodo);
-      // APPROACH 1: Invalidating The Cache
-      // queryClient.invalidateQueries({ queryKey: ['todos'] });
-      // APPROACH 2:
-      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [savedTodo, ...(todos || [])]);
+
+    onMutate: (newTodo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
+
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [newTodo, ...(todos || [])]);
       if (ref.current) ref.current.value = '';
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(['todos'], context.previousTodos);
     },
   });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
