5d75049b4f3a068db3de2365876803ace542936a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-18 16:36:45 +0600

    TodoForm

--- React Quary/src/react-query/TodoForm.tsx
+++ React Quary/src/react-query/TodoForm.tsx
@@ -1,10 +1,31 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import { useRef } from 'react';
+import { Todo } from '../hooks/useTodos';
 
 const TodoForm = () => {
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+      .post('https://jsonplaceholder.typicode.com/todos', todo)
+      .then((response) => response.data),
+  });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='row mb-3'>
+    <form
+      className='row mb-3'
+      onSubmit={(event) => {
+        if (!ref.current?.value) return;
+
+        event.preventDefault();
+        addTodo.mutate({
+          id: 0,
+          title: ref.current?.value,
+          completed: false,
+          userId: 1,
+        });
+      }}>
       <div className='col'>
         <input ref={ref} type='text' className='form-control' />
       </div>
