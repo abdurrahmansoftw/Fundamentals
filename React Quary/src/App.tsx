e1b12c133b0a2d042aff71b04d3973667c32f95b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-19 22:20:10 +0600

    Refactor

--- React Quary/src/App.tsx
+++ React Quary/src/App.tsx
@@ -1,25 +1,23 @@
 import { useReducer } from 'react';
-import AuthContext from './contexts/authContext';
 import TaskContext from './contexts/taskContext';
-import authReducer from './reducers/authReducer';
 import taskReducer from './reducers/taskReducer';
+import AuthProvider from './state-management/AuthProvider';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
 
 function App() {
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, '');
 
   return (
     <div className='container p-4'>
       <h1>Todo Form</h1>
 
-      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+      <AuthProvider>
         <TaskContext.Provider value={{ tasks, dispatch: taskDispatch }}>
           <NavBar />
           <HomePage />
         </TaskContext.Provider>
-      </AuthContext.Provider>
+      </AuthProvider>
     </div>
   );
 }
