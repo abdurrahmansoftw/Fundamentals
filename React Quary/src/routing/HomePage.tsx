25c823f7aa3ac24e5b8886be780d991e228a3d85
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 23:07:28 +0600

    HomePage

--- React Quary/src/routing/HomePage.tsx
+++ React Quary/src/routing/HomePage.tsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-  return (
-    <>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, mollitia!</p>
-      <Link to='/users'>Users</Link>
-    </>
-  );
-};
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+				mollitia!
+			</p>
+			<Link to='/users'>Users</Link>
+		</>
+	)
+}
 
-export default HomePage;
+export default HomePage
