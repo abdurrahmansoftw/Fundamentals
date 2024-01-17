f4b5964a6e2e2468b47e86980129d8874963ee4a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 23:07:00 +0600

    TodoList

--- React Quary/src/react-query/TodoList.tsx
+++ React Quary/src/react-query/TodoList.tsx
@@ -1,20 +1,20 @@
-import useTodos from '../hooks/useTodos';
+import useTodos from '../hooks/useTodos'
 
 const TodoList = () => {
-  const { isPending: loading, error, data: todos } = useTodos();
+	const { isPending: loading, error, data: todos } = useTodos()
 
-  if (loading) return 'Loading...';
-  if (error) return 'An error has occurred: ' + error.message;
+	if (loading) return 'Loading...'
+	if (error) return 'An error has occurred: ' + error.message
 
-  return (
-    <ul className='list-group'>
-      {todos?.map((todo) => (
-        <li key={todo.id} className='list-group-item'>
-          {todo.title}
-        </li>
-      ))}
-    </ul>
-  );
-};
+	return (
+		<ul className='list-group'>
+			{todos?.map((todo) => (
+				<li key={todo.id} className='list-group-item'>
+					{todo.title}
+				</li>
+			))}
+		</ul>
+	)
+}
 
-export default TodoList;
+export default TodoList
