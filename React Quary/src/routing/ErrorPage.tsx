58aff76cb48e6d2b2ba56c3b4d4c3bb6e6665ef9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 23:07:23 +0600

    ErrorPage

--- React Quary/src/routing/ErrorPage.tsx
+++ React Quary/src/routing/ErrorPage.tsx
@@ -1,15 +1,15 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
-  const error = useRouteError();
+	const error = useRouteError()
 
-  return (
-    <>
-      <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>{isRouteErrorResponse(error) ? 'Ivalid page' : 'Unexpected Error'}</p>
-    </>
-  );
-};
+	return (
+		<>
+			<h1>Oops...</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+			<p>{isRouteErrorResponse(error) ? 'Ivalid page' : 'Unexpected Error'}</p>
+		</>
+	)
+}
 
-export default ErrorPage;
+export default ErrorPage
