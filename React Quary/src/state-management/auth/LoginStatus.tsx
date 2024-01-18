975d8d14a8a2d3bb7ef395b15fb9402f93a61c79
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-18 09:59:59 +0600

    LoginStatus

--- React Quary/src/state-management/auth/LoginStatus.tsx
+++ React Quary/src/state-management/auth/LoginStatus.tsx
@@ -1,25 +1,25 @@
-import useAuthStore from './store';
+import useAuthStore from './store'
 
 const LoginStatus = () => {
-  const { user, login, logout } = useAuthStore();
-  if (user)
-    return (
-      <>
-        <div>
-          <span className='mx-2'>{user}</span>
-          <a onClick={() => logout()} href='#'>
-            Logout
-          </a>
-        </div>
-      </>
-    );
-  return (
-    <div>
-      <a onClick={() => login('akaid')} href='#'>
-        LOGIN
-      </a>
-    </div>
-  );
-};
+	const { user, login, logout } = useAuthStore()
+	if (user)
+		return (
+			<>
+				<div>
+					<span className='mx-2'>{user}</span>
+					<a onClick={() => logout()} href='#'>
+						Logout
+					</a>
+				</div>
+			</>
+		)
+	return (
+		<div>
+			<a onClick={() => login('akaid')} href='#'>
+				LOGIN
+			</a>
+		</div>
+	)
+}
 
-export default LoginStatus;
+export default LoginStatus
