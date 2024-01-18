9d63c8e11a5f5a795b7276b23f6495e807cd512a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-18 10:01:02 +0600

    taskReducer

--- React Quary/src/state-management/tasks/TaskProvider.tsx
+++ React Quary/src/state-management/tasks/TaskProvider.tsx
@@ -1,41 +1,45 @@
-import { ReactNode, useReducer } from 'react';
-import TaskContext from './taskContext';
+import { ReactNode, useReducer } from 'react'
+import TaskContext from './taskContext'
 
 export interface Task {
-  id: number;
-  title: string;
+	id: number
+	title: string
 }
 
 interface AddTask {
-  type: 'ADD';
-  payload: Task;
+	type: 'ADD'
+	payload: Task
 }
 
 interface DeleteTask {
-  type: 'DELETE';
-  taskId: number;
+	type: 'DELETE'
+	taskId: number
 }
 
-export type TaskAction = AddTask | DeleteTask;
+export type TaskAction = AddTask | DeleteTask
 
 const taskReducer = (tasks: Task[], action: TaskAction): Task[] => {
-  switch (action.type) {
-    case 'ADD':
-      return [...tasks, action.payload];
-    case 'DELETE':
-      return tasks.filter((task) => task.id !== action.taskId);
-    default:
-      return tasks;
-  }
-};
+	switch (action.type) {
+		case 'ADD':
+			return [...tasks, action.payload]
+		case 'DELETE':
+			return tasks.filter((task) => task.id !== action.taskId)
+		default:
+			return tasks
+	}
+}
 
 interface Props {
-  children: ReactNode;
+	children: ReactNode
 }
 
 const TaskProvider = ({ children }: Props) => {
-  const [tasks, dispatch] = useReducer(taskReducer, []);
-  return <TaskContext.Provider value={{ tasks, dispatch }}>{children}</TaskContext.Provider>;
-};
+	const [tasks, dispatch] = useReducer(taskReducer, [])
+	return (
+		<TaskContext.Provider value={{ tasks, dispatch }}>
+			{children}
+		</TaskContext.Provider>
+	)
+}
 
-export default TaskProvider;
+export default TaskProvider
