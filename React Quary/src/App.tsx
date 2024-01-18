b5dc2656e0e699507e291f934d58454bb51c70a3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-18 10:01:20 +0600

    App

--- React Quary/src/App.tsx
+++ React Quary/src/App.tsx
@@ -1,12 +1,12 @@
-import HomePage from './routing/HomePage';
+import HomePage from './routing/HomePage'
 
 function App() {
-  return (
-    <div className='container p-4'>
-      <h1>React Query</h1>
-      <HomePage />
-    </div>
-  );
+	return (
+		<div className='container p-4'>
+			<h1>React Query</h1>
+			<HomePage />
+		</div>
+	)
 }
 
-export default App;
+export default App
