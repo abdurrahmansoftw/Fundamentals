361bee36145df8f12ede20a9f4991bcb5545b671
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-18 10:00:55 +0600

    TaskList

--- React Quary/src/state-management/tasks/TaskList.tsx
+++ React Quary/src/state-management/tasks/TaskList.tsx
@@ -1,33 +1,45 @@
-import { useContext } from 'react';
-import useAuthStore from '../auth/store';
-import TaskContext from './taskContext';
+import { useContext } from 'react'
+import useAuthStore from '../auth/store'
+import TaskContext from './taskContext'
 
-const useTasks = () => useContext(TaskContext);
+const useTasks = () => useContext(TaskContext)
 
 const TaskList = () => {
-  const { tasks, dispatch } = useTasks();
-  const { user } = useAuthStore();
+	const { tasks, dispatch } = useTasks()
+	const { user } = useAuthStore()
 
-  return (
-    <>
-      <p>User: {user}</p>
-      <button
-        onClick={() => dispatch({ type: 'ADD', payload: { id: Date.now(), title: 'New Task' } })}
-        className='btn btn-primary my-3'>
-        Add Task
-      </button>
-      <ul className='list-group'>
-        {tasks.map((task) => (
-          <li key={task.id} className='list-group-item d-flex justify-content-between align-items-center'>
-            <span className='flex-grow-1'>{task.title}</span>
-            <button className='btn btn-outline-danger' onClick={() => dispatch({ type: 'DELETE', taskId: task.id })}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+	return (
+		<>
+			<p>User: {user}</p>
+			<button
+				onClick={() =>
+					dispatch({
+						type: 'ADD',
+						payload: { id: Date.now(), title: 'New Task' },
+					})
+				}
+				className='btn btn-primary my-3'
+			>
+				Add Task
+			</button>
+			<ul className='list-group'>
+				{tasks.map((task) => (
+					<li
+						key={task.id}
+						className='list-group-item d-flex justify-content-between align-items-center'
+					>
+						<span className='flex-grow-1'>{task.title}</span>
+						<button
+							className='btn btn-outline-danger'
+							onClick={() => dispatch({ type: 'DELETE', taskId: task.id })}
+						>
+							Delete
+						</button>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+}
 
-export default TaskList;
+export default TaskList
