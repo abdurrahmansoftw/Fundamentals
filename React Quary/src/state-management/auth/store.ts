f1b157984f1365950105f0f33b44b9b358602dbd
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-18 10:00:06 +0600

    useAuthStore

--- React Quary/src/state-management/auth/store.ts
+++ React Quary/src/state-management/auth/store.ts
@@ -1,15 +1,15 @@
-import { create } from 'zustand';
+import { create } from 'zustand'
 
 interface AuthState {
-  user: string;
-  login: (username: string) => void;
-  logout: () => void;
+	user: string
+	login: (username: string) => void
+	logout: () => void
 }
 
 const useAuthStore = create<AuthState>((set) => ({
-  user: '',
-  login: (username) => set(() => ({ user: username })),
-  logout: () => set(() => ({ user: '' })),
-}));
+	user: '',
+	login: (username) => set(() => ({ user: username })),
+	logout: () => set(() => ({ user: '' })),
+}))
 
-export default useAuthStore;
+export default useAuthStore
