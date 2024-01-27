bac15ac3b077ec837c1a0104c208f7213301fe42
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-01-27 23:10:51 +0600

    ContactPage

--- react-course-part2-starter/src/routing/ContactPage.tsx
+++ react-course-part2-starter/src/routing/ContactPage.tsx
@@ -1,14 +1,14 @@
 const ContactPage = () => {
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        // Redirect the user to the home page
-      }}
-    >
-      <button className="btn btn-primary">Submit</button>
-    </form>
-  );
-};
+	return (
+		<form
+			onSubmit={(event) => {
+				event.preventDefault()
+				// Redirect the user to the home page
+			}}
+		>
+			<button className='btn btn-primary'>Submit</button>
+		</form>
+	)
+}
 
-export default ContactPage;
+export default ContactPage
