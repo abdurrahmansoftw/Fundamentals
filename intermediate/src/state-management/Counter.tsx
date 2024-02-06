7eb4704bab67dc521443f7983884cd196420533a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-06 23:48:03 +0600

    update

--- intermediate/src/state-management/Counter.tsx
+++ intermediate/src/state-management/Counter.tsx
@@ -1,25 +1,22 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
 
   return (
     <div>
       Counter ({value})
       <button
         onClick={() => setValue(value + 1)}
-        className="btn btn-primary mx-1"
+        className='btn btn-primary mx-1'
       >
         Increment
       </button>
-      <button
-        onClick={() => setValue(0)}
-        className="btn btn-primary mx-1"
-      >
+      <button onClick={() => setValue(0)} className='btn btn-primary mx-1'>
         Reset
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter
--- intermediate/src/state-management/TaskList.tsx
+++ intermediate/src/state-management/TaskList.tsx
@@ -1,38 +1,33 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 interface Task {
-  id: number;
-  title: string;
+  id: number
+  title: string
 }
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([])
 
   return (
     <>
       <button
         onClick={() =>
-          setTasks([
-            { id: Date.now(), title: 'Task ' + Date.now() },
-            ...tasks,
-          ])
+          setTasks([{ id: Date.now(), title: 'Task ' + Date.now() }, ...tasks])
         }
-        className="btn btn-primary my-3"
+        className='btn btn-primary my-3'
       >
         Add Task
       </button>
-      <ul className="list-group">
+      <ul className='list-group'>
         {tasks.map((task) => (
           <li
             key={task.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
+            className='list-group-item d-flex justify-content-between align-items-center'
           >
-            <span className="flex-grow-1">{task.title}</span>
+            <span className='flex-grow-1'>{task.title}</span>
             <button
-              className="btn btn-outline-danger"
-              onClick={() =>
-                setTasks(tasks.filter((t) => t.id !== task.id))
-              }
+              className='btn btn-outline-danger'
+              onClick={() => setTasks(tasks.filter((t) => t.id !== task.id))}
             >
               Delete
             </button>
@@ -40,7 +35,7 @@ const TaskList = () => {
         ))}
       </ul>
     </>
-  );
-};
+  )
+}
 
-export default TaskList;
+export default TaskList
