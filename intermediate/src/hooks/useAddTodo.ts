75d697b46bdd3607d857b5e80157ddf55e90e242
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-06 23:29:33 +0600

    useAddTodo

---
+++ intermediate/src/hooks/useAddTodo.ts
@@ -0,0 +1,41 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import { Todo } from '../react-query/TodoList'
+
+interface AddTodoContext {
+  previusTodos: Todo[]
+}
+
+export const useAddTodo = () => {
+  const queryClient = useQueryClient()
+
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+        .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previusTodos = queryClient.getQueryData<Todo[]>(['todos']) || []
+
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ])
+      if (ref.current) return (ref.current.value = '')
+
+      return { previusTodos }
+    },
+
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      )
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return
+      queryClient.setQueryData<Todo[]>(['todos'], context?.previusTodos)
+    },
+  })
+}
