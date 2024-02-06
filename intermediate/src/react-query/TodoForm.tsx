723c16b0f891bdd4c718ff35c129689f33c74c45
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-06 22:12:10 +0600

    error handle

--- intermediate/src/react-query/TodoForm.tsx
+++ intermediate/src/react-query/TodoForm.tsx
@@ -6,7 +6,7 @@ import { Todo } from './TodoList'
 const TodoForm = () => {
   const queryClient = useQueryClient()
 
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
@@ -28,27 +28,32 @@ const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null)
 
   return (
-    <form
-      className='row mb-3'
-      onSubmit={(event) => {
-        event.preventDefault()
+    <div>
+      {addTodo.error && (
+        <div className='alert alert-danger'>{addTodo.error.message}</div>
+      )}
+      <form
+        className='row mb-3'
+        onSubmit={(event) => {
+          event.preventDefault()
 
-        if (ref.current && ref.current.value)
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            completed: false,
-            userId: 1,
-          })
-      }}
-    >
-      <div className='col-10'>
-        <input ref={ref} type='text' className='form-control ' />
-      </div>
-      <div className='col'>
-        <button className='btn btn-primary'>Add</button>
-      </div>
-    </form>
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            })
+        }}
+      >
+        <div className='col-10'>
+          <input ref={ref} type='text' className='form-control ' />
+        </div>
+        <div className='col'>
+          <button className='btn btn-primary'>Add</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
