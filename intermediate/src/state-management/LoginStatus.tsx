07ac9238be1b09202dfabcfd56e42d938a1333c7
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-07 11:57:30 +0600

    authReducer

--- intermediate/src/state-management/LoginStatus.tsx
+++ intermediate/src/state-management/LoginStatus.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+import authReducer from './reducers/authReducer'
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('')
+  const [user, dispatch] = useReducer(authReducer, '')
 
   if (user)
     return (
       <React.Fragment>
         <div>
           <span className='mx-2'>{user}</span>
-          <a onClick={() => setUser('')} href='#'>
+          <a onClick={() => dispatch({ type: 'LOGOUT' })} href='#'>
             Logout
           </a>
         </div>
@@ -16,7 +17,10 @@ const LoginStatus = () => {
     )
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href='#'>
+      <a
+        onClick={() => dispatch({ type: 'LOGIN', username: 'AbdurRahman' })}
+        href='#'
+      >
         Login
       </a>
     </div>
--- intermediate/src/state-management/reducers/authReducer.ts
+++ intermediate/src/state-management/reducers/authReducer.ts
@@ -1,3 +1,18 @@
-const authReducer = () => {}
+interface LoginAction {
+  type: 'LOGIN'
+  username: string
+}
+
+interface LogoutAction {
+  type: 'LOGOUT'
+}
+
+type AuthAction = LoginAction | LogoutAction
+
+const authReducer = (state: string, action: AuthAction): string => {
+  if (action.type === 'LOGIN') return action.username
+  if (action.type === 'LOGOUT') return ''
+  return state
+}
 
 export default authReducer
